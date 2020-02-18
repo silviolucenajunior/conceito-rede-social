4280aff84c9f5fdcc3e02152a38a5f9e5a26691c
Silvio Lucena Junior <silvio.lucena.junior@intrapoint.com>
2020-02-19 09:48:00 +1300

    basic file upload working

--- server/server.js
+++ server/server.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
 const cors = require('cors');
 const server = express();
 
 server.use(cors());
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(fileUpload({
+    createParentPath: true
+}));
 
 server.post('/register-person', function(request, response) {
     console.log("Registering a new person in database");
+    console.log(request.files.file);
+    console.log("****");
+
+    response.sendStatus(200);
 });
 
 server.get('/register-person', function(request, response) {
